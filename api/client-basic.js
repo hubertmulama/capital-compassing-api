ad809b8ebd373339ba874661dd454be386559c59
hubertmulama <hubert.mulama@gmail.com>
2025-10-12 19:06:28 +0300

    Update client-basic.js

--- api/client-basic.js
+++ api/client-basic.js
@@ -1,5 +1,5 @@
 
-import { getConnection } from '../../lib/database.js';
+import { getDbConfig } from './_db-config.js';
 import mysql from 'mysql2/promise';
 
 
@@ -33,15 +33,6 @@ export default async function handler(req, res) {
       database: "freedb_Capital compassing", 
     });
 */
-  let connection;
-  try {
-    // Direct database configuration (temporary)
-    connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
 
     
     const [clientRows] = await connection.execute(
