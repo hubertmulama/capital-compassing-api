582c2e0c1c5c0259c2e9016ef13e7ffc937b02ea
hubertmulama <hubert.mulama@gmail.com>
2025-10-12 19:08:51 +0300

    Update _db-config.js

--- api/_db-config.js
+++ api/_db-config.js
@@ -1,10 +1,9 @@
 // /api/_db-config.js
 export const getDbConfig = () => {
   return {
-    const connection = await mysql.createConnection({
-      host: "sql.freedb.tech",         // e.g., sqlXXX.infinityfree.com
-      user: "freedb_Hubert_mulama",
-      password: "#?wqa5T4m5GB%JB",
-      database: "freedb_Capital compassing",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   };
 };
