b08086d9fda16fa95b2daaefcb35182fdfceb2fe
hubertmulama <hubert.mulama@gmail.com>
2025-10-19 12:07:48 +0300

    Update account-details.js

--- api/account-details.js
+++ api/account-details.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  console.log('=== RAW REQUEST INFO ===');
+  console.log('=== RAW REQUESTq INFO ===');
   console.log('req.body:', req.body);
 
   let body = '';
